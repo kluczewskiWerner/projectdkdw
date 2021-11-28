c7186f09deb21402a03e50edd7b8f556fec0e86e
Dawid Kluczewski <notedeagle@gmail.com>
2021-11-28 16:32:24 +0100

    Fixed LoginControllerTest

--- src/test/java/pl/kluczewski/currency_converter/controller/LoginControllerTest.java
+++ src/test/java/pl/kluczewski/currency_converter/controller/LoginControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -20,21 +21,32 @@ class LoginControllerTest {
     @Autowired
     private MockMvc mvc;
 
+    public void prepareUser() throws Exception {
+        mvc.perform(post("/register").content("{\"email\": \"test@test.com\", \"password\": \"test\"}")
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andDo(print())
+                .andExpect(status().is(200))
+                .andReturn();
+    }
+
     @Test
     void shouldLoginAndGetContent() throws Exception {
-        MvcResult login = mvc.perform(post("/login").content("{\"username\": \"test\", \"password\": \"test\"}"))
+        prepareUser();
+
+        MvcResult login = mvc.perform(post("/login")
+                        .content("{\"email\": \"test@test.com\", \"password\": \"test\"}"))
                 .andDo(print())
-                .andExpect(status().is(200))            //status 200
-                .andReturn();//czy wartośc jest poprawna
+                .andExpect(status().is(200))
+                .andReturn();
         String token = login.getResponse().getHeader("Authorization");
 
         mvc.perform(get("/secured").header("Authorization", token))
                 .andDo(print())
-                .andExpect(status().is(200))            //status 200
+                .andExpect(status().is(200))
                 .andExpect(content().string("secured"));
 
         mvc.perform(get("/secured"))
                 .andDo(print())
-                .andExpect(status().is(401));          //usunięty token
+                .andExpect(status().is(401));
     }
 }
