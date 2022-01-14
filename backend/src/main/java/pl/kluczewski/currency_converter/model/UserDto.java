fa8222546685fdb94bd67aa94a287567e072bdca
Dawid Kluczewski <notedeagle@gmail.com>
2022-01-14 20:50:46 +0100

    Fix

--- backend/src/main/java/pl/kluczewski/currency_converter/model/UserDto.java
+++ backend/src/main/java/pl/kluczewski/currency_converter/model/UserDto.java
@@ -1,4 +1,11 @@
 package pl.kluczewski.currency_converter.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserDto {
+    private String lastName;
+    private String firstName;
 }
